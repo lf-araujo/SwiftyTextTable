4bb2d142824bb27fb01724636b52adfc8ecd8121
luis <ldearaujo@unimelb.edu.au>
2018-08-28 20:30:36 -0300

    another attempt

--- Package.swift
+++ Package.swift
@@ -3,7 +3,10 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftyTextTable",
+     products: [
+        .library(name: "SwiftyTextTable", targets: ["SwiftyTextTable"])
+    ],
   targets: [
-    .target(name: "SwiftyTextTable"),
+    .target(name: "SwiftyTextTable")
   ]
 )
